95699338006aa80d7de8f5b6eb32b7e5cde209ae
Pesala winodith <gkpesalawinodith@gmail.com>
2024-11-05 11:14:26 +0530

    frontend updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
-// frontend/src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ProductList from './components/ProductList';
-import ProductForm from './components/ProductForm';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    return (
-        <Router>
-            <div>
-                <Routes>
-                    <Route path="/" element={<ProductList />} />
-                    <Route path="/add" element={<ProductForm />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
